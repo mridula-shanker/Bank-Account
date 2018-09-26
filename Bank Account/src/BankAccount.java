d0fa0a6bd09c95fd98ee9a1b378a2ec353f97943
mshanker22 <mshanker22@HS-MATH2-06.gvsd.org>
2018-09-26 13:40:46 -0400

    homework

---
+++ Bank Account/src/BankAccount.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author mshanker22
+ *
+ */
+public class BankAccount 
+{
+	private String name;
+	private int  accNum;
+	private double balance;
+	
+	public BankAccount(String n, int aN)
+	{
+		balance= 0;
+		
+	}
+}
